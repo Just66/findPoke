77956090b3c7dc4ab3a543a41eb5e9db488bed62
Dmytro Aprelenko <d.aprelenko@gmail.com>
2017-02-26 16:35:07 +0200

    installed GeoFire & Firebase

---
+++ findPoke/findPoke-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  findPoke-Bridging-Header.h
+//  findPoke
+//
+//  Created by Dmytro Aprelenko on 26.02.17.
+//  Copyright © 2017 Dmytro Aprelenko. All rights reserved.
+//
+
+#import "GeoFire.h"
